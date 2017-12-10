27479beacf2453c262f51cad56b799005f07acac
leakeyash <123661285@qq.com>
2017-12-11 00:03:28 +0800

    add a project to test some method

---
+++ MethodTest/MethodTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MethodTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var xobject = new TestObject("henry","fool");
+            var hobject = new TestObject("henry", "fool");
+            
+            var objects = new List<TestObject>
+            {
+                hobject,
+                new TestObject("robert","genius")  
+            };
+            var hindex = objects.IndexOf(hobject);
+            var xindex = objects.IndexOf(xobject);
+            var index = objects.Exists(x => x.Name == xobject.Name && x.Value == xobject.Name);
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ MethodTest/MethodTest/TestObject.cs
@@ -0,0 +1,33 @@
+﻿namespace MethodTest
+{
+    public class TestObject
+    {
+        public TestObject(string name, string value)
+        {
+            Name = name;
+            Value = value;
+        }
+
+        public string Name { get; }
+        public string Value { get; }
+
+        public override bool Equals(object obj)
+        {
+            var o = obj as TestObject;
+            return o != null && Equals(o);
+        }
+
+        protected bool Equals(TestObject other)
+        {
+            return string.Equals(Name, other.Name) && string.Equals(Value, other.Value);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((Name != null ? Name.GetHashCode() : 0) * 397) ^ (Value != null ? Value.GetHashCode() : 0);
+            }
+        }
+    }
+}
